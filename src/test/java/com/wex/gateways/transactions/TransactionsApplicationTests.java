2ed7dbe280406f0868e327de47aa763b9c1f4d85
Gustavo-BuilderPrime <gustavo@builderprime.com>
2023-11-29 18:11:40 -0300

    Initial commit

---
+++ src/test/java/com/wex/gateways/transactions/TransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wex.gateways.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

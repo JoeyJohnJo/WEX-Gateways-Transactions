dfe86480a21dc4765123d1877e920e31c2b55bc9
Gustavo-BuilderPrime <gustavo@builderprime.com>
2023-11-30 16:37:03 -0300

    Added more exception handlers and error messages

--- src/main/java/com/wex/gateways/transactions/app/domain/exceptions/ExceptionResolver.java
+++ src/main/java/com/wex/gateways/transactions/app/domain/exceptions/ExceptionResolver.java
@@ -1,11 +1,19 @@
 package com.wex.gateways.transactions.app.domain.exceptions;
 
+import com.wex.gateways.transactions.app.config.i18n.Localization;
 import com.wex.gateways.transactions.app.domain.exceptions.errors.InvalidValueException;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Objects;
+import java.util.Optional;
 
 @RestControllerAdvice
 public class ExceptionResolver {
@@ -15,5 +23,24 @@ public class ExceptionResolver {
     public ResponseEntity<ErrorResponseBody> invalidValueException(InvalidValueException exception) {
         return ResponseEntity.badRequest()
             .body(new ErrorResponseBody(exception.getFaultyValue(), exception.getLocalizedMessage()));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseBody> methodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        return ResponseEntity.badRequest().body(new ErrorResponseBody(
+            Optional.ofNullable(exception.getFieldError()).map(FieldError::getField).orElse(""),
+            Localization.getExceptionMessageForLocale("exception.invalidValue",
+                LocaleContextHolder.getLocale())
+        ));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponseBody> httpMessageNotReadableException(HttpMessageNotReadableException unused) {
+        return ResponseEntity.badRequest().body(new ErrorResponseBody(null,
+            Localization.getExceptionMessageForLocale("exception.httpMessageNotReadableException",
+                LocaleContextHolder.getLocale())
+        ));
     }
 }
